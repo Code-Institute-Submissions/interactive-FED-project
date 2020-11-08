ad7a987a131ce34a57ef94717a3c64a57fa9f418
Pedro Luiz Cantarelli Junior <pcantarelli@hotmail.com>
2020-11-08 02:33:19 +0000

    Added http request to brewery API using input value

--- assets/js/find_breweries.js
+++ assets/js/find_breweries.js
@@ -36,29 +36,84 @@ function initMap() {
 }
 
 
-function searchBreweries(location) {
-    var map = new google.maps.Map(document.getElementById("map"), initialCoordinates);
+function searchBreweries(event) {
 
-    //Create marker function
-    function createMarker(breweryData) {
-        let marker = new google.maps.Marker({
-            position: breweryData.coords,
-            map: map,
-            //icon: breweryData.icon = 'url.png'
-        });  
-    
-        let mapInfoContainer = new google.maps.InfoWindow({
-            content: infoContainerContent(breweryData.name)
-        })
-    
-        marker.addListener('click', function() {
-            mapInfoContainer.open(map, marker);
-        })
-    
-    }
+    let location = $('#input-location').val().toLowerCase().replace(" ", "_");
+    let breweriesToMark= [];
 
-    let input = $('#input-location').val();
-    console.log(input);
-    createMarker(dataSmithwick);
+    //Result verification
+    //If input empty => reset
+
+    //else
+        //loading animation
+
+        //look for data
+       
+        $.when(
+            //$.getJSON(`apiURL${location}`)
+            $.getJSON(`https://api.openbrewerydb.org/breweries?by_city=${location}`)
+        ).then(
+            function(response) {
+                var breweriesResponse = response;
+                breweriesResponse.forEach(function(element) {
+                    if (element.name && element.latitude && element.longitude) {
+                        console.log(element.name);
+                        breweriesToMark.push(element);                 
+                    }
+                });
+            }, function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $('.error-message').html(`<p>No brewery found in ${location}</p>`)
+                } else {
+                    console.log(errorResponse);
+                    $('.error-message').html(`<p>Error ${errorResponse.responseJSON.message}</p>`)
+                }
+            }
+        )
+
+        //If data empty => reset
+        //If data => create markers
+
+        
+   
+        var map = new google.maps.Map(document.getElementById("map"), initialCoordinates);
+        
+        
+        //Create marker function
+        function createMarker(breweryData) {
+            let marker = new google.maps.Marker({
+                position: {
+                    lat: parseFloat(breweryData.latitude),
+                    lng: parseFloat(breweryData.longitude)
+                },
+                map: map,
+                //icon: breweryData.icon = 'url.png'
+            });  
+        
+            let mapInfoContainer = new google.maps.InfoWindow({
+                content: infoContainerContent(breweryData.name)
+            })
+        
+            marker.addListener('click', function() {
+                mapInfoContainer.open(map, marker);
+            })
+        
+        }
+        
+        setTimeout(function() {
+            // console.log(breweriesToMark)
+            // console.log(breweriesToMark[0])
+            // console.log(breweriesToMark[0].latitude)
+            // console.log(typeof(breweriesToMark[0].latitude))
+            
+            createMarker(breweriesToMark[0])
+
+        },3000)
+        
+        
+
+        var san = 'San Diego';
+  
+    // createMarker(/* Usar a função para criar os markers */);
 }
 
